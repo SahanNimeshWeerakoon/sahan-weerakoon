07866edc23ec25834571ea8a666ac8df8cb98732
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2023-11-07 23:29:10 +0530

    remove animation test

--- app/components/miniComponents/MovingIcon.tsx
+++ app/components/miniComponents/MovingIcon.tsx
@@ -1,4 +1,18 @@
+import { animated, useSpring } from '@react-spring/web'
+
 const MovingIcon = ({ source, iconName, width, height, left, top, mainIcon } : { source: string, iconName: string, width: number, height: number, left: number, top: number, mainIcon: boolean }) => {
+    const [ springs, api ] = useSpring(() => ({
+        from: { x: 0 }
+    }));
+
+    const handleClick = () => {
+        api.start({
+            from: { x: 0 },
+            to: [{ x: 100 }, { x: 0 }],
+            loop: true
+        })
+    }
+    
     return (
         <img
             className={`movingIcon ${mainIcon ? 'main' : ''}`}
@@ -12,6 +26,16 @@ const MovingIcon = ({ source, iconName, width, height, left, top, mainIcon } : {
                 left: `${left}%`, top: `${top}%`,
                 animationName: `${mainIcon ? 'breathIcon' : 'moveIcon'}`
             }} />
+        // <animated.div
+        //     onClick={handleClick}
+        //     style={{
+        //         width: 80,
+        //         height: 80,
+        //         background: '#ff6d6d',
+        //         borderRadius: 8,
+        //         ...springs
+        //     }}
+        // />
     );
 }
 
