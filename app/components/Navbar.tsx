6e95484207ae11a78977c0839a0786bf6cd4d8b9
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2025-09-24 21:54:58 +0530

    clean the folders to go for the version 2

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Button from "./miniComponents/Button";
 
-const NavBar = ({ toggleContactModal }: { toggleContactModal: any }) => {
+const NavBar = () => {
     const [isMobileNavActive, setIsMobileNavActive] = useState(false);
     return (
         <nav className="navbar">
@@ -12,14 +12,12 @@ const NavBar = ({ toggleContactModal }: { toggleContactModal: any }) => {
                 <div className="navbar_links">
                     <img src="/img/Hamberger.png" alt="Nav menu icon" title="Nav menu icon" onClick={() => {setIsMobileNavActive(state => !state)}} />
                     <ul>
-                        <li><a href="#" onClick={() => toggleContactModal("toggleModal")}>CONTACT ME</a></li>
                         <li><a href="#about">ABOUT ME</a></li>
                         <li><Button btnTxt="PROJECTS" url="#projects" /></li>
                     </ul>
                 </div>
             </div>
             <ul className={`navbar_mobile ${isMobileNavActive ? 'active' : ''}`}>
-                <li><a href="#" onClick={() => toggleContactModal("toggleModal")}>CONTACT ME</a></li>
                 <li><a href="#about">ABOUT ME</a></li>
                 <li><Button btnTxt="PROJECTS" url="#projects" /></li>
             </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,19 @@
 "use client"
 import { useState } from "react";
 import NavBar from "./components/Navbar";
-import Banner from "./components/Banner";
 import SocialMediaList from "./components/SocialMediaList";
-import Projects from "./components/Projects";
-import Modal from "./components/Modal";
-import Alert from "./components/miniComponents/Alert";
-import ContactForm from "./components/miniComponents/ContactForm";
 
 export default function Home() {
-  const [ isContactFormOpen, setIsContactFormOpen ] = useState(false);
-  const [ formType, setFormType ] = useState("contact");
-  const [ emailSent, setEmailSent ] = useState(true);
   const [ socialMediaList ] = useState([
     { name: "LinkedIn", link: "https://www.linkedin.com/in/sahan-weerakoon-software-engineer", source: "/img/LinkedIn.png" },
     { name: "GitHub", link: "https://www.github.com/SahanNimeshWeerakoon", source: "/img/Github.png" },
     { name: "Medium", link: "https://medium.com/@hsn.weerakoon.m", source: "/img/Medium.png" },
   ]);
 
-  const toggleContactModal = (closeStatus: string) => {
-    console.log(closeStatus)
-    if(closeStatus == "toggleModal") {
-      setIsContactFormOpen(status => !status);
-    } else if (closeStatus === "sent") {
-      setFormType("successful");
-      setEmailSent(true);
-      setTimeout(() => { setIsContactFormOpen(false) }, 4000);
-    } else if(closeStatus === "notSent") {
-      setFormType("failed");
-      setEmailSent(false);
-      setTimeout(() => { setIsContactFormOpen(false) }, 4000);
-    }
-  }
-
   return (
     <main>
-      <NavBar toggleContactModal={toggleContactModal} />
-      <Banner />
+      <NavBar />
       <SocialMediaList list={socialMediaList} />
-      <Projects />
-      <Modal isOpen={isContactFormOpen} onClose={toggleContactModal}>
-        {formType==="contact" ?
-          <ContactForm onClose={toggleContactModal} /> :
-          <Alert success={emailSent}> { emailSent ? "Thank you for contacting me. I will get back to you ASAP" : "Sorry something went wrong." } </Alert>
-        }
-      </Modal>
     </main>
   )
 }
